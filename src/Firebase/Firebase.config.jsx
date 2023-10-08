2bef01a83f3d8a2b53028b9e74ee25291d91d3c0
Bushra01-Dhaka <humayraanjum87@gmail.com>
2023-10-09 01:58:13 +0600

    Updated firebase config

--- src/Firebase/Firebase.config.jsx
+++ src/Firebase/Firebase.config.jsx
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyANC-OEWg9ee5CQh3XczpQnnjzQgYlGN_Y",
-  authDomain: "wedding-planner-810f8.firebaseapp.com",
-  projectId: "wedding-planner-810f8",
-  storageBucket: "wedding-planner-810f8.appspot.com",
-  messagingSenderId: "1017632645345",
-  appId: "1:1017632645345:web:d60ede4dd042c905c4d7a0"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID ,
+  appId: import.meta.env.VITE_APPID 
 };
 
 // Initialize Firebase
